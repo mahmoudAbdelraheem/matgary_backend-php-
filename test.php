333530f29fadd8adf67d969ddffb330637b81820
mahmoudAbdelraheem <mmoud2031@gmail.com>
2024-02-01 14:16:58 +0200

    testing sign up & vrefiy code

--- test.php
+++ test.php
@@ -1,7 +1,11 @@
 <?php 
-$to = 'mmoud2031@gmail.com';
-$title= 'hi';
-$body = 'from back end test.php';
-$header = 'From: support@memo.com' . '\n' . 'CC:mmoud2031@gmail.com';
-mail($to,$title,$body,$header);
+// $to = 'mmoud2031@gmail.com';
+// $title= 'hi';
+// $body = 'from back end test.php';
+// $header = 'From: support@memo.com' . '\n' . 'CC:mmoud2031@gmail.com';
+// mail($to,$title,$body,$header);
 
+
+include "connect.php";
+
+getAllData("users" ,"1=1");
