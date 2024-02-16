286c371363f25ad2ee5cce30896e8c49a9f6efe7
mahmoudAbdelraheem <mmoud2031@gmail.com>
2024-02-16 15:08:28 +0200

    item discount

--- items/itemsview.php
+++ items/itemsview.php
@@ -7,11 +7,11 @@ $userId = filterRequest('userid');
 
 //?getAllData('items1view' , "cate_id = $categoryId");
 
-$stmt = $con->prepare("SELECT items1view.*,1 AS favorite_item FROM items1view
+$stmt = $con->prepare("SELECT items1view.*,1 AS favorite_item ,(item_price - (item_price * item_discount /100)) as item_discount_price FROM items1view
 INNER JOIN favorite ON favorite.favorite_items_id = items1view.item_id AND favorite.favorite_users_id = $userId
 WHERE cate_id = $categoryId
 UNION ALL 
-SELECT *, 0 AS favorite_item FROM items1view
+SELECT *, 0 AS favorite_item ,(item_price - (item_price * item_discount /100)) as item_discount_price FROM items1view
 WHERE cate_id = $categoryId AND item_id NOT IN (SELECT items1view.item_id FROM items1view
 INNER JOIN favorite ON favorite.favorite_items_id = items1view.item_id AND favorite.favorite_users_id = $userId)");
 
--- view.sql
+++ view.sql
@@ -21,6 +21,6 @@ INNER JOIN items ON items.item_id = favorite.favorite_items_id
 
 /* cart user items view*/
 CREATE OR REPLACE VIEW cartview AS
-SELECT SUM(items.item_price) AS itemprice ,COUNT(cart_item_id) AS itemcount , cart.* , items.* FROM cart
+SELECT SUM(items.item_price - (items.item_price * items.item_discount /100)) AS itemprice ,COUNT(cart_item_id) AS itemcount , cart.* , items.* FROM cart
 INNER JOIN items ON items.item_id =cart.cart_item_id
 GROUP BY cart.cart_item_id , cart.cart_user_id
